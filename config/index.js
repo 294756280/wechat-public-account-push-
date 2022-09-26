ef79ff573ff1a08c01ff7338f5b1a6ccffcbed37
294756280 <114373278+294756280@users.noreply.github.com>
2022-09-26 15:45:54 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -61,7 +61,7 @@ export const config = {
   USERS: ["of37a6aelOJyTCM9dbEzAGfI083Q"
     {
       // 想要发送的人的名字
-      name: "1",
+      name: "",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oGm3m5sV1zyTuEs-TyZcRu-XE8eg",
       // 你想对他发送的模板消息的模板ID
@@ -71,7 +71,7 @@ export const config = {
       // 所在城市
       city: "阳江",
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '今日',
+      horoscopeDate: '',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
@@ -87,12 +87,11 @@ export const config = {
       ],
       // 专属纪念日/倒数
 日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
-      customizedDateList: [{"keyword": "love_day", date: "2022-03-05"}
+      //customizedDateList: [{"keyword": "love_day", date: "2022-03-05"}
         // 在一起的日子
         {"keyword": "love_day", date: "2022-09-08"},
         // 结婚纪念日
         {"keyword": "marry_day", date: "2022-09-09"},
         // 退伍日
         {"keyword": "ex_day", date: "2022-09-10"},
-      ]
 }
