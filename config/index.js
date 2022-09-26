4b57c9fa2d9ccb9946564bd50c77548519ee47e4
294756280 <114373278+294756280@users.noreply.github.com>
2022-09-26 11:45:31 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,14 +6,14 @@ export const config = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "wx2e723d877b82140e",
+  APP_ID: "wx6f98ab6f4bdf6179",
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "45b5f66b8d9abd6c9f67eed2cec0af61",
-  TEHPLATE_ID:"Mo1MuGIw-0-2O_FqLlS58NiMd_5ThBrGq_ZdK_6KtWU",
-  USERS:["of37a6aelOJyTCM9dbEzAGfI083Q"],
+  APP_SECRET: "fa3e985523abae056af22005c39c3bef",
+  TEHPLATE_ID:"	GIkVauDJ85xYv1oc3XJj5E6rFJMtWRMrLTa80RAoxKo",
+  USERS:["oGm3m5hBuDCyVm0XlpyATtRWmnMo"],
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
   IS_SHOW_COLOR: true,
