5f8a9bc4305b209be9ef5566c9c24720a22bbf21
294756280 <114373278+294756280@users.noreply.github.com>
2022-09-26 16:40:05 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ config = [
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "wx6f98ab6f4bdf6179",
+  APP_ID:"wx6f98ab6f4bdf6179",
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "fa3e985523abae056af22005c39c3bef",
+  APP_SECRET:"fa3e985523abae056af22005c39c3bef",
   TEHPLATE_ID:"W9fn6EPvKWistNprJvwMap1WB1tUPAw7apLxWEV2GY0",
   USERS:["oGm3m5sV1zyTuEs-TyZcRu-XE8eg"],
   // 是否给文字设置多彩颜色, 和emoji不兼容
